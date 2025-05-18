248b1cd611975cc0274f0234b1a77f1481d6cd10
hp0912 <809211365@qq.com>
2025-05-18 11:21:05 +0800

    feat: 全局配置

--- controller/global_settings.go
+++ controller/global_settings.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"wechat-robot-client/model"
 	"wechat-robot-client/pkg/appx"
+	"wechat-robot-client/pkg/utils"
 	"wechat-robot-client/service"
 
 	"github.com/gin-gonic/gin"
@@ -32,6 +33,102 @@ func (ct *GlobalSettings) SaveGlobalSettings(c *gin.Context) {
 	if ok, err := appx.BindAndValid(c, &req); !ok || err != nil {
 		resp.ToErrorResponse(errors.New("参数错误"))
 		return
+	}
+	if req.ChatAIEnabled != nil && *req.ChatAIEnabled {
+		if req.ChatAPIKey == "" || req.ChatBaseURL == "" || req.ChatModel == "" || req.ChatPrompt == "" {
+			resp.ToErrorResponse(errors.New("参数错误"))
+			return
+		}
+	}
+	if req.ImageAIEnabled != nil && *req.ImageAIEnabled {
+		if req.ImageModel == "" || req.ImageAISettings == nil {
+			resp.ToErrorResponse(errors.New("参数错误"))
+			return
+		}
+	}
+	if req.WelcomeEnabled != nil && *req.WelcomeEnabled {
+		if req.WelcomeType == "" {
+			resp.ToErrorResponse(errors.New("参数错误"))
+			return
+		}
+		if req.WelcomeType == model.WelcomeTypeText {
+			if req.WelcomeText == "" {
+				resp.ToErrorResponse(errors.New("参数错误"))
+				return
+			}
+		}
+		if req.WelcomeType == model.WelcomeTypeEmoji {
+			if req.WelcomeEmojiMD5 == "" || req.WelcomeEmojiLen == 0 {
+				resp.ToErrorResponse(errors.New("参数错误"))
+				return
+			}
+		}
+		if req.WelcomeType == model.WelcomeTypeImage {
+			if req.WelcomeImageURL == "" {
+				resp.ToErrorResponse(errors.New("参数错误"))
+				return
+			}
+		}
+		if req.WelcomeType == model.WelcomeTypeURL {
+			if req.WelcomeText == "" || req.WelcomeURL == "" {
+				resp.ToErrorResponse(errors.New("参数错误"))
+				return
+			}
+		}
+	}
+	if req.ChatRoomRankingEnabled != nil && *req.ChatRoomRankingEnabled {
+		if req.ChatRoomRankingDailyCron == "" {
+			resp.ToErrorResponse(errors.New("参数错误"))
+			return
+		}
+		if req.ChatRoomRankingDailyCron != "" {
+			if !utils.IsDailyAtHourMinute(req.ChatRoomRankingDailyCron) {
+				resp.ToErrorResponse(errors.New("参数错误"))
+				return
+			}
+		}
+		if req.ChatRoomRankingWeeklyCron != nil && *req.ChatRoomRankingWeeklyCron != "" {
+			if !utils.IsWeeklyMondayAtHourMinute(*req.ChatRoomRankingWeeklyCron) {
+				resp.ToErrorResponse(errors.New("参数错误"))
+				return
+			}
+		}
+		if req.ChatRoomRankingMonthCron != nil && *req.ChatRoomRankingMonthCron != "" {
+			if !utils.IsMonthly1stAtHourMinute(*req.ChatRoomRankingMonthCron) {
+				resp.ToErrorResponse(errors.New("参数错误"))
+				return
+			}
+		}
+	}
+	if req.ChatRoomSummaryEnabled != nil && *req.ChatRoomSummaryEnabled {
+		if req.ChatRoomSummaryModel == "" || req.ChatRoomSummaryCron == "" {
+			resp.ToErrorResponse(errors.New("参数错误"))
+			return
+		}
+		if !utils.IsDailyAtHourMinute(req.ChatRoomSummaryCron) {
+			resp.ToErrorResponse(errors.New("参数错误"))
+			return
+		}
+	}
+	if req.NewsEnabled != nil && *req.NewsEnabled {
+		if req.NewsType == "" || req.NewsCron == "" {
+			resp.ToErrorResponse(errors.New("参数错误"))
+			return
+		}
+		if !utils.IsDailyAtHourMinute(req.NewsCron) {
+			resp.ToErrorResponse(errors.New("参数错误"))
+			return
+		}
+	}
+	if req.MorningEnabled != nil && *req.MorningEnabled {
+		if req.MorningCron == "" {
+			resp.ToErrorResponse(errors.New("参数错误"))
+			return
+		}
+		if !utils.IsDailyAtHourMinute(req.MorningCron) {
+			resp.ToErrorResponse(errors.New("参数错误"))
+			return
+		}
 	}
 	service.NewGlobalSettingsService(c).SaveGlobalSettings(&req)
 	resp.ToResponse(nil)
--- model/global_settings.go
+++ model/global_settings.go
@@ -1,43 +1,57 @@
 package model
 
 import (
-	"database/sql"
-
 	"gorm.io/datatypes"
 )
 
+type WelcomeType string
+
+const (
+	WelcomeTypeText  WelcomeType = "text"  // 文本
+	WelcomeTypeEmoji WelcomeType = "emoji" // 表情
+	WelcomeTypeImage WelcomeType = "image" // 图片
+	WelcomeTypeURL   WelcomeType = "url"   // 链接
+)
+
+type NewsType string
+
+const (
+	NewsTypeText  NewsType = "text"  // 文本
+	NewsTypeImage NewsType = "image" // 图片
+)
+
 type GlobalSettings struct {
-	ID                        uint64         `gorm:"column:id;primaryKey;autoIncrement;comment:公共配置表主键ID"`
-	Owner                     string         `gorm:"column:owner;type:varchar(64);default:'';comment:所有者微信ID"`
-	ChatAIEnabled             bool           `gorm:"column:chat_ai_enabled;default:false;comment:是否启用AI聊天功能"`
-	ChatAITrigger             string         `gorm:"column:chat_ai_trigger;type:varchar(20);default:'';comment:触发聊天AI的关键词"`
-	ChatBaseURL               string         `gorm:"column:chat_base_url;type:varchar(255);default:'';comment:聊天AI的基础URL地址"`
-	ChatAPIKey                string         `gorm:"column:chat_api_key;type:varchar(255);default:'';comment:聊天AI的API密钥"`
-	ChatModel                 string         `gorm:"column:chat_model;type:varchar(100);default:'';comment:聊天AI使用的模型名称"`
-	ChatPrompt                sql.NullString `gorm:"column:chat_prompt;type:text;comment:聊天AI系统提示词"`
-	ImageAIEnabled            bool           `gorm:"column:image_ai_enabled;default:false;comment:是否启用AI绘图功能"`
-	ImageModel                string         `gorm:"column:image_model;type:varchar(255);default:'';comment:绘图AI模型"`
-	ImageAISettings           datatypes.JSON `gorm:"column:image_ai_settings;type:json;comment:绘图AI配置项"`
-	WelcomeEnabled            bool           `gorm:"column:welcome_enabled;default:false;comment:是否启用新成员加群欢迎功能"`
-	WelcomeType               string         `gorm:"column:welcome_type;type:enum('text','emoji','image','url');default:'text';comment:欢迎方式：text-文本，emoji-表情，image-图片，url-链接"`
-	WelcomeText               string         `gorm:"column:welcome_text;type:varchar(255);default:'';comment:欢迎新成员的文本"`
-	WelcomeEmojiMD5           string         `gorm:"column:welcome_emoji_md5;type:varchar(64);default:'';comment:欢迎新成员的表情MD5"`
-	WelcomeEmojiLen           int64          `gorm:"column:welcome_emoji_len;default:0;comment:欢迎新成员的表情MD5长度"`
-	WelcomeImageURL           string         `gorm:"column:welcome_image_url;type:varchar(255);default:'';comment:欢迎新成员的图片URL"`
-	WelcomeURL                string         `gorm:"column:welcome_url;type:varchar(255);default:'';comment:欢迎新成员的URL"`
-	ChatRoomRankingEnabled    bool           `gorm:"column:chat_room_ranking_enabled;default:false;comment:是否启用群聊排行榜功能"`
-	ChatRoomRankingDailyCron  string         `gorm:"column:chat_room_ranking_daily_cron;type:varchar(255);default:'';comment:每日定时任务表达式"`
-	ChatRoomRankingWeeklyCron string         `gorm:"column:chat_room_ranking_weekly_cron;type:varchar(255);default:'';comment:每周定时任务表达式"`
-	ChatRoomRankingMonthCron  string         `gorm:"column:chat_room_ranking_month_cron;type:varchar(255);default:'';comment:每月定时任务表达式"`
-	ChatRoomSummaryEnabled    bool           `gorm:"column:chat_room_summary_enabled;default:false;comment:是否启用聊天记录总结功能"`
-	ChatRoomSummaryModel      string         `gorm:"column:chat_room_summary_model;type:varchar(100);default:'';comment:聊天总结使用的AI模型名称"`
-	ChatRoomSummaryCron       string         `gorm:"column:chat_room_summary_cron;type:varchar(100);default:'';comment:群聊总结的定时任务表达式"`
-	NewsEnabled               bool           `gorm:"column:news_enabled;default:false;comment:是否启用每日早报功能"`
-	NewsType                  string         `gorm:"column:news_type;type:enum('text','image');default:'text';comment:是否启用每日早报功能"`
-	NewsCron                  string         `gorm:"column:news_cron;type:varchar(100);default:'';comment:每日早报的定时任务表达式"`
-	MorningEnabled            bool           `gorm:"column:morning_enabled;default:false;comment:是否启用早安问候功能"`
-	MorningCron               string         `gorm:"column:morning_cron;type:varchar(100);default:'';comment:早安问候的定时任务表达式"`
-	FriendSyncCron            string         `gorm:"column:friend_sync_cron;type:varchar(100);default:'';comment:好友同步的定时任务表达式"`
+	ID                        uint64         `gorm:"column:id;primaryKey;autoIncrement;comment:公共配置表主键ID" json:"id"`
+	Owner                     string         `gorm:"column:owner;type:varchar(64);default:'';comment:所有者微信ID" json:"owner"`
+	ChatAIEnabled             *bool          `gorm:"column:chat_ai_enabled;default:false;comment:是否启用AI聊天功能" json:"chat_ai_enabled"`
+	ChatAITrigger             *string        `gorm:"column:chat_ai_trigger;type:varchar(20);default:'';comment:触发聊天AI的关键词" json:"chat_ai_trigger"`
+	ChatBaseURL               string         `gorm:"column:chat_base_url;type:varchar(255);default:'';comment:聊天AI的基础URL地址" json:"chat_base_url"`
+	ChatAPIKey                string         `gorm:"column:chat_api_key;type:varchar(255);default:'';comment:聊天AI的API密钥" json:"chat_api_key"`
+	ChatModel                 string         `gorm:"column:chat_model;type:varchar(100);default:'';comment:聊天AI使用的模型名称" json:"chat_model"`
+	ChatPrompt                string         `gorm:"column:chat_prompt;type:text;comment:聊天AI系统提示词" json:"chat_prompt"`
+	ImageAIEnabled            *bool          `gorm:"column:image_ai_enabled;default:false;comment:是否启用AI绘图功能" json:"image_ai_enabled"`
+	ImageModel                string         `gorm:"column:image_model;type:varchar(255);default:'';comment:绘图AI模型" json:"image_model"`
+	ImageAISettings           datatypes.JSON `gorm:"column:image_ai_settings;type:json;comment:绘图AI配置项" json:"image_ai_settings"`
+	WelcomeEnabled            *bool          `gorm:"column:welcome_enabled;default:false;comment:是否启用新成员加群欢迎功能" json:"welcome_enabled"`
+	WelcomeType               WelcomeType    `gorm:"column:welcome_type;type:enum('text','emoji','image','url');default:'text';comment:欢迎方式：text-文本，emoji-表情，image-图片，url-链接" json:"welcome_type"`
+	WelcomeText               string         `gorm:"column:welcome_text;type:varchar(255);default:'';comment:欢迎新成员的文本" json:"welcome_text"`
+	WelcomeEmojiMD5           string         `gorm:"column:welcome_emoji_md5;type:varchar(64);default:'';comment:欢迎新成员的表情MD5" json:"welcome_emoji_md5"`
+	WelcomeEmojiLen           int64          `gorm:"column:welcome_emoji_len;default:0;comment:欢迎新成员的表情MD5长度" json:"welcome_emoji_len"`
+	WelcomeImageURL           string         `gorm:"column:welcome_image_url;type:varchar(255);default:'';comment:欢迎新成员的图片URL" json:"welcome_image_url"`
+	WelcomeURL                string         `gorm:"column:welcome_url;type:varchar(255);default:'';comment:欢迎新成员的URL" json:"welcome_url"`
+	ChatRoomRankingEnabled    *bool          `gorm:"column:chat_room_ranking_enabled;default:false;comment:是否启用群聊排行榜功能" json:"chat_room_ranking_enabled"`
+	ChatRoomRankingDailyCron  string         `gorm:"column:chat_room_ranking_daily_cron;type:varchar(255);default:'';comment:每日定时任务表达式" json:"chat_room_ranking_daily_cron"`
+	ChatRoomRankingWeeklyCron *string        `gorm:"column:chat_room_ranking_weekly_cron;type:varchar(255);default:'';comment:每周定时任务表达式" json:"chat_room_ranking_weekly_cron"`
+	ChatRoomRankingMonthCron  *string        `gorm:"column:chat_room_ranking_month_cron;type:varchar(255);default:'';comment:每月定时任务表达式" json:"chat_room_ranking_month_cron"`
+	ChatRoomSummaryEnabled    *bool          `gorm:"column:chat_room_summary_enabled;default:false;comment:是否启用聊天记录总结功能" json:"chat_room_summary_enabled"`
+	ChatRoomSummaryModel      string         `gorm:"column:chat_room_summary_model;type:varchar(100);default:'';comment:聊天总结使用的AI模型名称" json:"chat_room_summary_model"`
+	ChatRoomSummaryCron       string         `gorm:"column:chat_room_summary_cron;type:varchar(100);default:'';comment:群聊总结的定时任务表达式" json:"chat_room_summary_cron"`
+	NewsEnabled               *bool          `gorm:"column:news_enabled;default:false;comment:是否启用每日早报功能" json:"news_enabled"`
+	NewsType                  NewsType       `gorm:"column:news_type;type:enum('text','image');default:'text';comment:是否启用每日早报功能" json:"news_type"`
+	NewsCron                  string         `gorm:"column:news_cron;type:varchar(100);default:'';comment:每日早报的定时任务表达式" json:"news_cron"`
+	MorningEnabled            *bool          `gorm:"column:morning_enabled;default:false;comment:是否启用早安问候功能" json:"morning_enabled"`
+	MorningCron               string         `gorm:"column:morning_cron;type:varchar(100);default:'';comment:早安问候的定时任务表达式" json:"morning_cron"`
+	FriendSyncCron            string         `gorm:"column:friend_sync_cron;type:varchar(100);default:'';comment:好友同步的定时任务表达式" json:"friend_sync_cron"`
 }
 
 // TableName 设置表名
