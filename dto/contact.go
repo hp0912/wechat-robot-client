b582571c47608e967b924dc166149b389d92c3b4
hp0912 <809211365@qq.com>
2025-06-28 22:05:46 +0800

    fix: 修复联系人参数异常

--- dto/contact.go
+++ dto/contact.go
@@ -1,7 +1,7 @@
 package dto
 
 type ContactListRequest struct {
-	ContactIDs []string `form:"contact_ids[]" json:"contact_ids"`
+	ContactIDs []string `form:"contact_ids" json:"contact_ids"`
 	Type       string   `form:"type" json:"type"`
 	Keyword    string   `form:"keyword" json:"keyword"`
 }
--- dto/moments.go
+++ dto/moments.go
@@ -1,11 +1,11 @@
 package dto
 
 type FriendCircleGetListRequest struct {
-	FristPageMd5 string `json:"frist_page_md5" binding:"required"`
-	MaxID        string `json:"max_id" binding:"required"`
+	FristPageMd5 string `form:"frist_page_md5" json:"frist_page_md5"`
+	MaxID        string `form:"max_id" json:"max_id" binding:"required"`
 }
 
 type DownFriendCircleMediaRequest struct {
-	Url string `json:"url" binding:"required"`
-	Key string `json:"key"`
+	Url string `form:"url" json:"url" binding:"required"`
+	Key string `form:"key" json:"key"`
 }
