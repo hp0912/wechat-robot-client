20d99ef391d7a96bed99d6eaceb6e3ec563a7642
hp0912 <809211365@qq.com>
2026-04-09 22:55:01 +0800

    feat: gorm auto migrate

--- model/moment_settings.go
+++ model/moment_settings.go
@@ -2,7 +2,7 @@ package model
 
 type MomentSettings struct {
 	ID                  int64   `gorm:"primaryKey;autoIncrement;comment:表主键ID" json:"id"`
-	SyncKey             string  `gorm:"type:text;comment:朋友圈同步Key" json:"sync_key"`
+	SyncKey             string  `gorm:"type:text;not null;default:'';comment:朋友圈同步的key" json:"sync_key"`
 	AutoLike            *bool   `gorm:"default:false;comment:开启自动点赞" json:"auto_like"`
 	AutoComment         *bool   `gorm:"default:false;comment:开启自动评论" json:"auto_comment"`
 	Whitelist           *string `gorm:"type:text;comment:自动点赞、评论白名单" json:"whitelist"`
@@ -11,7 +11,7 @@ type MomentSettings struct {
 	AIAPIKey            string  `gorm:"type:varchar(255);default:'';comment:AI的API密钥" json:"ai_api_key"`
 	WorkflowModel       string  `gorm:"type:varchar(100);default:'';comment:工作流模型" json:"workflow_model"`
 	CommentModel        string  `gorm:"type:varchar(100);default:'';comment:评论模型" json:"comment_model"`
-	CommentPrompt       string  `gorm:"type:text;comment:评论系统提示词" json:"comment_prompt"`
+	CommentPrompt       string  `gorm:"type:text;not null;default:'';comment:评论系统提示词" json:"comment_prompt"`
 	MaxCompletionTokens *int    `gorm:"default:0;comment:评论最大回复" json:"max_completion_tokens"`
 }
 
